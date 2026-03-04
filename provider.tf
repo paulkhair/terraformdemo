e2ac608808636f8c68c478da6a135e4d1ef0257a
Paul Faltas <paulnaeem@gmail.com>
2026-03-04 15:21:55 +0100

    github local automation

--- provider.tf
+++ provider.tf
@@ -1,27 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.46.0"
-    }     
-  }
-  
-  backend "azurerm" {
-        resource_group_name  = "tfstaterg0001"
-        storage_account_name = "tfstatesta0001"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-        subscription_id = "*********"
-        tenant_id       = "*********"
     }
+  }
+  # No backend block here means it uses the "local" backend
 }
+
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-  subscription_id = "*********"
-  tenant_id       = "*********"
+  features {}
+  # Credentials will be handled by Environment Variables in GitHub Actions
 }
